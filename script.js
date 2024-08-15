d583947a37c59d4898a347359264d7f7b2c1b0c0
Maanlicht91 <aynurdemir1991@hotmail.com>
2024-08-16 02:28:09 +0300

    Added Clear UI State

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const form = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 const clearBtn = document.getElementById("clear");
+const filter = document.getElementById("filter");
 
 //* Add Items
 function addItem(e) {
@@ -24,6 +25,9 @@ function addItem(e) {
 
   //! Clear input after added item
   itemInput.value = "";
+
+  //! Reset UI
+  checkUI();
 }
 
 //* Create Button and Icon
@@ -51,7 +55,12 @@ function createIcon(classes) {
 //! Remove item
 function removeItem(e) {
   if (e.target.classList.contains("fa-xmark")) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+
+    //! Reset UI
+    checkUI();
   }
 }
 
@@ -60,6 +69,18 @@ function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+
+  //! Reset UI
+  checkUI();
+}
+
+//* ResetUI or checkUI - remove filter and clear button when none item
+
+function checkUI() {
+  const hasItem = itemList.children.length;
+  // const hasItem = itemList.firstElementChild;
+  filter.style.display = hasItem ? "block" : "none";
+  clearBtn.style.display = hasItem ? "block" : "none";
 }
 
 //0 Add Item
@@ -68,3 +89,5 @@ form.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 //0 Clear All
 clearBtn.addEventListener("click", clearItems);
+//0 Reset UI
+checkUI();
